59046471979fda60a3805fdfdd69e01fd1eff390
Lion <61917380@qq.com>
2019-01-28 17:03:04 +0800

    add checkDB

--- Sources/CLDB/NSObject+CLDB.m
+++ Sources/CLDB/NSObject+CLDB.m
@@ -9,6 +9,8 @@
 #import "NSObject+CLDB.h"
 #import <objc/runtime.h>
 
+#import <FMDB.h>
+
 @implementation NSObject (CLDB)
 
 +(instancetype)CL_objectForId:(id)primaryKey{
@@ -21,11 +23,39 @@
     NSArray *arr = [self getSaveProperties];
     for (NSString *key in arr) {
         id value = [self valueForKey:key];
+        
     }
 }
 
 
 #pragma mark - private methods
+-(void)checkDB{
+    //1.创建database路径
+    NSString *docuPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    NSString *dbPath = [docuPath stringByAppendingPathComponent:@"test.db"];
+    NSLog(@"!!!dbPath = %@",dbPath);
+    //2.创建对应路径下数据库
+    FMDatabase *db = [FMDatabase databaseWithPath:dbPath];
+    //3.在数据库中进行增删改查操作时，需要判断数据库是否open，如果open失败，可能是权限或者资源不足，数据库操作完成通常使用close关闭数据库
+    [db open];
+    if (![db open]) {
+        NSLog(@"db open fail");
+        return;
+    }
+    //4.数据库中创建表（可创建多张）
+    NSString *sql = [NSString stringWithFormat:@"create table if not exists t_student ('ID' INTEGER PRIMARY KEY AUTOINCREMENT,'name' TEXT NOT NULL, 'phone' TEXT NOT NULL,'score' INTEGER NOT NULL)"];
+    for (NSString *p in [self getSaveProperties]) {
+        //TODO:
+    }
+    
+    //5.执行更新操作 此处database直接操作，不考虑多线程问题，多线程问题，用FMDatabaseQueue 每次数据库操作之后都会返回bool数值，YES，表示success，NO，表示fail,可以通过 @see lastError @see lastErrorCode @see lastErrorMessage
+    BOOL result = [db executeUpdate:sql];
+    if (result) {
+        NSLog(@"create table success");
+        
+    }
+    [db close];
+}
 -(void)insert{
     
 }
@@ -55,15 +85,15 @@
 }
 
 #pragma mark - db name
--(NSString *)CL_dbName{
+-(NSString *)dbName{
     NSString *bundleId = [[NSBundle mainBundle] bundleIdentifier];
     NSString *dbName = [bundleId stringByReplacingOccurrencesOfString:@"." withString:@""];
     return dbName;
 }
--(NSString *)CL_tableName{
+-(NSString *)tableName{
     return NSStringFromClass([self class]);
 }
--(NSString *)CL_primaryKey{
+-(NSString *)primaryKey{
     return @"nid";
 }
 
