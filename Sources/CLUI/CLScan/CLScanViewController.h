86839a0fe04409b13d0c92508ecf26feb00430e1
lion <61917380@qq.com>
2019-04-08 16:06:50 +0800

    add CLScanViewController

---
+++ Sources/CLUI/CLScan/CLScanViewController.h
@@ -0,0 +1,17 @@
+//
+//  CLScanViewController.h
+//  CLKits
+//
+//  Created by lion on 2019/4/8.
+//  Copyright © 2019 chenl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLScanViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
